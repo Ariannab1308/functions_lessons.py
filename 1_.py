d03590ac4078c9fde27cb6f8a7879581facdee87
Ariannab1308 <ariannab1308@gmail.com>
2026-01-27 20:29:05 +0000

    week24

--- 1_.py
+++ 1_.py
@@ -1,3 +1,67 @@
+# finction = A block of reuseable code
+    # place () after the function name to invoke it
+
+# def happy_birthday(name, age): # define functon (peramiter)
+#     print("Happy Birthday to {age}!")
+#     print("You are {age} years old!")
+#     print("Happy Birthday to you!")
+#     print()
+
+# happy_birthday("Bro" , 20) # call/envoke function - (argument) - real data
+# happy_birthday("Steve" , 30)
+# happy_birthday("Joe" , 40)
+
+# --------------------------------
+
+# def display_invoice(username, amount, due_date):
+#     print(F"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("JoeSchmo", 100.01, "01/02")
+# display_invoice("MartyYa", 200.97, "02/18")
+# display_invoice("CarlaOh", 67.67, "01/30")
+
+# --------------------------------
+
+# return = statement used to end a function\
+#     and send back a result to the caller
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract (x, y):
+#     z = x - y
+#     return z
+
+# def multiply (x, y):
+#     z = x * y
+#     return z
+
+# def divide (x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+# --------------------------------
+
+# def create_name(first, last):
+#     first = frist.capitalize()
+#     last = last.capitalize()
+#     return first + " " + last
+
+# full_name = create_name("spongebob", "squarepamts")
+
+
+# print(full_name)
+
+# --------------------------------
+
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
